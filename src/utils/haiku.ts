845c7fcbb12820329a22b36e737c826ffa780296
Andrew Panassiouk <andrew.panassiouk@gmail.com>
2021-02-22 18:54:39 +0200

    Reworked syllable count function

--- src/utils/haiku.ts
+++ src/utils/haiku.ts
@@ -1,20 +1,22 @@
+function syllableCountEn(text: string): number {
+	let word = text.toLowerCase();
+	if (word.length <= 3) {
+		return 1;
+	}
+	word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
+	word = word.replace(/^y/, '');
+	return word.match(/[aeiouy]{1,2}/g)?.length ?? 0;
+}
+
+function syllableCountCyrillic(text: string): number {
+	return text.match(/[аеёиоуыэюяіїє]/ig)?.length ?? 0;
+}
+
+export const syllableCount = (text: string): number => syllableCountEn(text) + syllableCountCyrillic(text);
+
 /**
  * https://github.com/vitalyavolyn/haiku-detector/blob/main/detector.js
  */
-
-import { syllabify } from 'syllables-ru';
-
-const vowelsRegex = /[аеёиоуыэюя]/ig;
-
-const syllableCount = (text: string) => syllabify(text)
-	.replace(/\s+/g, ' ')
-	.split(' ')
-	.reduce((acc, v) => acc + (
-		v.split('·').length === 1
-		// Пропускаем предлоги и т.п. без гласных
-			? (v.match(vowelsRegex) ? 1 : 0)
-			: v.split('·').length
-	), 0);
 
 export const getHaiku = (text: string | null): string | null => {
 	if (!text) return null;
